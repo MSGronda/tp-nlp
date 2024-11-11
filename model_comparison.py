cd3467f2f70b9884acd2a9a0b03cdda4cfccfbff
MarcosCasiraghi <mcasiraghi@itba.edu.ar>
2024-11-11 08:00:12 -0300

    refactor: code rearangement

--- model_comparison.py
+++ model_comparison.py
@@ -3,21 +3,17 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 from sklearn.preprocessing import LabelEncoder
 
-# Load the Excel file
 file_path = './dataset/ManualTweetClassification.xlsx'
 data = pd.read_excel(file_path, skiprows=1)
 
-# List of model columns
 model_columns = ['bert', 'bertweet', 'distilbert', 'roberta', 'vader']
 true_column = 'classification'
 
-# Encode labels
 label_encoder = LabelEncoder()
 data[true_column] = label_encoder.fit_transform(data[true_column])
 for model in model_columns:
     data[model] = label_encoder.transform(data[model])
 
-# Initialize dictionaries to store metrics
 metrics = {model: {} for model in model_columns}
 
 # Calculate metrics for each model
@@ -27,7 +23,6 @@ for model in model_columns:
     metrics[model]['f1_score'] = f1_score(data[true_column], data[model], average='weighted')
     metrics[model]['accuracy'] = accuracy_score(data[true_column], data[model])
 
-# Plot bar graphs for each metric
 metrics_df = pd.DataFrame(metrics).T
 metrics_df.plot(kind='bar', figsize=(10, 6))
 plt.title('Model Performance Metrics')
--- subsetting.py
+++ subsetting.py
@@ -5,7 +5,7 @@ output_file_path = "./dataset/random_sample_1000.csv"
 
 df = pd.read_csv(input_file_path)
 
-df_sample = df.iloc[1:].sample(n=1000, random_state=42)  # Exclude first row, set random_state for reproducibility
+df_sample = df.iloc[1:].sample(n=1000, random_state=42)
 
 df_sample = pd.concat([df.iloc[[0]], df_sample])
 
